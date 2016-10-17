ee8b6a5fd1c3c3de12afd42ba14c5d95bab69e89
Haley Zeng <hzeng@stuy.edu>
2016-10-16 21:15:55 -0400

    fixed that bug

--- myTunez.c
+++ myTunez.c
@@ -13,7 +13,7 @@ song_node * create_node(char *name1, char *artist1){
   strcpy(new->name,name1);
   strcpy(new->artist,artist1);
   new->next = 0;
-  return 0;
+  return new;
 }
 
 song_node * insert_front(song_node *s,char *name1,char *artist1) {
@@ -22,7 +22,7 @@ song_node * insert_front(song_node *s,char *name1,char *artist1) {
   return new;
 }
 
-song_node * in_order(song_node *s,char * name1,char * artist1) {
+song_node * in_order(song_node *s,char * name1, char * artist1) {
   if (strcmp(s->name, name1) > 0) return insert_front(s,name1,artist1);
   song_node *retVal=s;
   song_node *bef=s;
@@ -37,7 +37,7 @@ song_node * in_order(song_node *s,char * name1,char * artist1) {
     bef = bef->next;
   }
   song_node * toInsert = create_node(name1, artist1);
-  s->next = toInsert;
+  bef->next = toInsert;
   return retVal;
 }
 
@@ -60,10 +60,11 @@ int main() {
 
   s=insert_front(s,"twinkle twinkle", "anon");
   print_list( s );
-  s=in_order(s,"uuu","a");
-  print_list( s );
   s=in_order(s,"happy birthday","guest");
-
+  print_list( s );
+  s=in_order(s,"ppy birthday","guest");
+  print_list( s );
+  s=in_order(s,"uuu","a");
   print_list( s );
   
   return 0;
