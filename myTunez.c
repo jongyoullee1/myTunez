5da5c1e80e358067e73b805dab804da893077405
Haley Zeng <hzeng@stuy.edu>
2016-10-16 21:35:56 -0400

    added search for song by title and testing in main

--- myTunez.c
+++ myTunez.c
@@ -41,6 +41,15 @@ song_node * in_order(song_node *s,char * name1, char * artist1) {
   return retVal;
 }
 
+song_node * find_song(song_node *list, char * song_title){
+  while (list){
+    if (strcmp(list->name, song_title) == 0)
+      break;
+    list = list->next;
+  }
+  return list;
+}
+
 void print_list(song_node *n ) {
 
   printf("[ ");
@@ -56,16 +65,38 @@ int main() {
 
   song_node *s = 0;
 
-  print_list(s );
-
+  printf("\n");
+  printf("========================================\n");
+  printf("\n");
+  printf("Testing printing and inserting in order:\n");
+  printf("At the beginning:\n");
+  print_list(s);
+  printf("Adding \"twinkle twinkle\":\n");
   s=insert_front(s,"twinkle twinkle", "anon");
-  print_list( s );
+  print_list(s);
+  printf("Adding \"happy birthday\":\n");
   s=in_order(s,"happy birthday","guest");
-  print_list( s );
-  s=in_order(s,"ppy birthday","guest");
-  print_list( s );
+  print_list(s);
+  printf("Adding \"purple\":\n");
+  s=in_order(s,"purple","guest");
+  print_list(s);
+  printf("Adding \"uuu\":\n");
   s=in_order(s,"uuu","a");
-  print_list( s );
+  print_list(s);
+  printf("\n");
+  printf("========================================\n");
+  printf("\n");
+  printf("Testing searching for songs by title:\n");
+  printf("Finding \"twinkle twinkle\" in s:\n");
+  print_list(find_song(s, "twinkle twinkle"));
+  printf("Finding \"uuu\" in s:\n");
+  print_list(find_song(s, "uuu"));
+  printf("Finding \"doesn't exist boi\" in s:\n");
+  print_list(find_song(s, "doesn't exist boi"));
+  printf("\n");
+  printf("========================================\n");
+  printf("\n");
+
   
   return 0;
 }
